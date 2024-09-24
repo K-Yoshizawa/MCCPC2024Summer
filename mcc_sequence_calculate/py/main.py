f69317b084e9b67a25e66f29ba30929ff60dae84
K-Yoshizwa <naniyoshi.z@gmail.com>
2024-09-24 14:32:31 +0900

    Python 解の追加

---
+++ mcc_sequence_calculate/py/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# Python Solution (writer)
+
+N = int(input())
+A = list(map(int, input().split(' ')))
+
+ans = 0
+cum = 0
+cnt = [0 for _ in range(N + 1)]
+for i in range(N - 1, -1, -1):
+    ans += cum - (cnt[A[i]] - 1) * cnt[A[i]] // 2
+    cum += cnt[A[i]]
+    cnt[A[i]] += 1
+print(ans)
