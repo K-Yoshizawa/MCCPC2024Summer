dcaf507aa52ee60eac70121a0af2a03495074bea
K-Yoshizwa <naniyoshi.z@gmail.com>
2024-09-24 13:57:26 +0900

    Python 解の追加

---
+++ worship/py/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# Python Solution (writer)
+
+A, B, C = map(int, input().split(' '))
+
+for i in range(A):
+    print('bow')
+for i in range(B):
+    print('clap')
+for i in range(C):
+    print('bow')
